b3ef6e9aa8fff2777b35b229032be6ffce79e85b
blahkheart <dannithomx@gmail.com>
2020-06-03 22:45:08 +0100

    added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="indigo darken-4 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon[0]"
+            class="mx-4 white--text"
+            icon
+            route   
+            :to="icon[1]"
+            target="_blank"
+          >
+            <v-icon size="24px">{{ icon[0] }}</v-icon>
+          </v-btn>
+        </v-card-text>
+  
+        <v-card-text class="white--text pt-0">
+            We’re a digital marketing and advisory firm located in Port Harcourt, Nigeria 
+            and serving clients nationally and internationally. Driven by the success of our clients, 
+            we take great pride in the online strategies we help you implement to drive 
+            website traffic.
+        </v-card-text>
+  
+        <v-divider></v-divider>
+  
+        <v-card-text class="white--text">
+         <v-icon>copyright</v-icon> <strong>{{ new Date().getFullYear() }} CreekWaters</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {
+    data: ()=> ({
+        icons: [
+            ['mdi-facebook', 'https://facebook.com/creekwatersdigital'],
+            ['mdi-twitter', 'twitter.com'],
+            ['mdi-instagram', 'https://instagram.com']
+        ]
+    })
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,59 @@
 <template>
   <div class="home">
-   <Header/>
-   <v-container>
-     <v-row>
-       <v-col>
-         <div class="text-center home-section">
-           <h2>THE FASTEST WAY TO INCREASE YOUR REVENUE</h2>
-           <p>
-             Our digital marketing agency, based in Northern Virginia, bridges the gap for businesses that don’t have the people, resources, time or expertise to get a measurable return on digital marketing.
-           </p>
-           <v-divider></v-divider>
-         </div>
-       </v-col>
-     </v-row>
-     <v-row
-        align="center"
-        justify="center"
-      >
-       <v-col>
-         <div class="text-center home-section">
-           <h2>
-            ARE YOU STRUGGLING WITH MAKING DIGITAL MARKETING WORK FOR YOUR BUSINESS?
-           </h2>
-           <v-divider></v-divider>    
-           <SiteVideo/> 
-         </div>
-       </v-col>
-     </v-row>
-   </v-container>
+    <Header/>
+    <v-container>
+      <v-row>
+        <v-col>
+          <div class="text-center home-section">
+            <h2>THE FASTEST WAY TO INCREASE YOUR REVENUE</h2>
+            <p>
+              Our digital marketing agency, based in Northern Virginia, bridges the gap for businesses that don’t have the people, resources, time or expertise to get a measurable return on digital marketing.
+            </p>
+            <v-divider></v-divider>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row
+          align="center"
+          justify="center"
+        >
+        <v-col>
+          <div class="text-center home-section">
+            <h2>
+              ARE YOU STRUGGLING WITH MAKING DIGITAL MARKETING WORK FOR YOUR BUSINESS?
+            </h2>
+            <v-divider></v-divider>    
+            <SiteVideo/> 
+          </div>
+        </v-col>
+      </v-row>
+        <v-row
+          align="center"
+          justify="center"
+        >
+        <v-col>
+          <div class="text-center home-section">
+              <v-btn large rounded class="mt-5">
+                Get a free social media audit for your business
+              </v-btn>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Header from '@/components/home/Header'
+import Footer from '@/components/Footer'
 import SiteVideo from '@/components/home/SiteVideo'
 export default {
   name: 'Home',
   components: {
     Header,
-    SiteVideo
+    SiteVideo,
+    Footer
   }
 }
 </script>
